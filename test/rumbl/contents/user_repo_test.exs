743f68f26ae31e09e74c6a8f661a78f37f01908c
jechol <jechol@skelterlabs.com>
2017-10-19 22:36:30 +0900

    page 152

---
+++ test/rumbl/contents/user_repo_test.exs
@@ -0,0 +1,16 @@
+defmodule Rumbl.Contents.UserRepoTest do
+  use Rumbl.DataCase
+  alias Rumbl.Accounts.User
+
+  @valid_attrs %{name: "A User", username: "eva"}
+
+  test "converts unique_constraint on username to error" do
+    insert_user(%{username: "eric"})
+    attrs = Map.put(@valid_attrs, :username, "eric")
+    changeset = User.changeset(%User{}, attrs)
+
+    assert changeset.valid?
+    assert {:error, changeset} = Repo.insert(changeset)
+    assert "has already been taken" in errors_on(changeset).username
+  end
+end
