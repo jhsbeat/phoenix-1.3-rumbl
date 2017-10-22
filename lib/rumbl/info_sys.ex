dd98d30e57f8aae5f34590d1460cb303478053f5
jechol <jechol@skelterlabs.com>
2017-10-22 10:35:31 +0900

    page 224

--- lib/rumbl/info_sys.ex
+++ lib/rumbl/info_sys.ex
@@ -23,13 +23,19 @@ defmodule Rumbl.InfoSys do
   defp spawn_query(backend, query, limit) do
     query_ref = make_ref()
     opts = [backend, query, query_ref, self(), limit]
+    # Supervisor 밑에서 시작하기 때문에 crash 하더라도 현재 프로세스에 영향을 주지 않음.
+    # restart: :temporary 기 때문에, die 하더라도 재시작하지 않음.
     {:ok, pid} = Supervisor.start_child(Rumbl.InfoSys.Supervisor, opts)
     monitor_ref = Process.monitor(pid)
     {pid, monitor_ref, query_ref}
   end
 
-  defp await_results(children, _opts) do
-    await_result(children, [], :infinity)
+  defp await_results(children, opts) do
+    timeout = opts[:timeout] || 5_000
+    timer = Process.send_after(self(), :timedout, timeout)
+    results = await_result(children, [], :infinity)
+    cleanup(timer)
+    results
   end
 
   defp await_result([head | tail], acc, timeout) do
@@ -41,8 +47,29 @@ defmodule Rumbl.InfoSys do
         await_result(tail, results ++ acc, timeout)
       {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
         await_result(tail, acc, timeout)
+      :timedout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
+    after timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
     end
   end
 
   defp await_result([], acc, _), do: acc
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    :erlang.cancel_timer(timer)
+    receive do
+      :timedout ->
+        :ok
+    after 0 ->
+        :ok
+    end
+  end
 end
