13e4bffd432026d2a2d7451ee4c6ad23c0454698
jechol <jechol@skelterlabs.com>
2017-10-26 23:59:24 +0900

    page 254

--- apps/rumbl/test/rumbl_web/channels/video_channel_test.exs
+++ apps/rumbl/test/rumbl_web/channels/video_channel_test.exs
@@ -22,4 +22,14 @@ defmodule RumblWeb.Channels.VideoChannelTest do
     assert socket.assigns.video_id == vid.id
     assert %{annotations: [%{body: "one"}, %{body: "two"}]} = reply
   end
+
+  test "inserting new annotations", %{socket: socket, video: vid} do
+    {:ok, _, socket} = subscribe_and_join(socket, "videos:#{vid.id}", %{})
+    # Phoenix.ChannelTest.push
+    ref = push(socket, "new_annotation", %{body: "the body", at: 0})
+    # assert_reply(ref, status, payload): ref 에 해당하는 push가 status, payload 로 응답했음을 assert (status, payload 둘 다 pattern 임)
+    assert_reply ref, :ok, %{}
+    # assert_broadcast(event, payload): event, payload는 패턴임
+    assert_broadcast "new_annotation", %{}
+  end
 end
