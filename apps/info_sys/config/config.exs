b735937ade32510ea7f177307e8f5d538298af08
jechol <jechol@skelterlabs.com>
2017-10-26 17:34:59 +0900

    page 246

--- apps/info_sys/config/config.exs
+++ apps/info_sys/config/config.exs
@@ -27,6 +27,6 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-# import_config "#{Mix.env}.exs"
+import_config "#{Mix.env}.exs"
 
 config :info_sys, :wolfram, app_id: "7QWU8G-TWL4RGQE4X"
